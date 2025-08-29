3346c276ef19eefaf430da9f17036a0634082b92
Jasminy Duarte <jasminyduuarte_@hotmail.com>
2025-08-29 18:33:53 -0400

    Update LinksApp with new structure and components

---
+++ src/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+import { colors } from "@/styles/colors";
+
+export default function Layout() {
+  const backgroundColor = colors.gray[950];
+  return (
+    <Stack 
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor }
+      }}
+    >
+        <Stack.Screen name="index" />
+    </Stack>
+  );
+}
+
+
---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text,View } from "react-native"
+
+import { styles } from "@/styles/index-styles"
+
+export default function Index() {
+  return (
+    <View style={ styles.container }>
+      <Text style={ styles.title }>Hello, jasminy!!</Text>
+      <Text style={ styles.title }>development mobile</Text>
+    </View>
+  )
+}
